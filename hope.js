df0730529e3531b570125b805582bbf0c8d4ae9d
Unknown <RetroPhoenix1831@gmail.com>
2017-07-18 22:35:39 -0400

    Massive command update
    Most commands now start with h!

--- hope.js
+++ hope.js
@@ -11,14 +11,14 @@ bot.on('message', (message) => {
 
 bot.on('message', (message) => {
 
-    if(message.content == 'give me carbon prices') {
+    if(message.content == 'h!prices carbon') {
         message.reply('this is what I have. Carbon: **10** <:Bux:329395951003369476>| **200K** <:Gas:329395933949198337> | **400K-600K** <:Minerals:329395908901076992>');
     }
 });
 
 bot.on('message', (message) => {
 
-    if(message.content == 'give me gold prices') {
+    if(message.content == 'h!prices gold') {
         message.reply('this is what I have. Gold: **20** <:Bux:329395951003369476>|*Undetermined* <:Gas:329395933949198337>| **850K-1M** <:Minerals:329395908901076992>');
     }
 
@@ -26,7 +26,7 @@ bot.on('message', (message) => {
 
 bot.on('message', (message) => {
 
-    if(message.content == 'give me titanium prices') {
+    if(message.content == 'h!prices titanium') {
         message.reply('this is what I have. Titanium: **9-12** <:Bux:329395951003369476>| **250K** <:Gas:329395933949198337> | **850K** <:Minerals:329395908901076992>');
     }
 
@@ -34,7 +34,7 @@ bot.on('message', (message) => {
 
 bot.on('message', (message) => {
 
-    if(message.content == 'give me silicon prices') {
+    if(message.content == 'h!prices silicon') {
         message.reply('this is what I have. Silicon: **25-30** <:Bux:329395951003369476>| **500K** <:Gas:329395933949198337> | **1M** <:Minerals:329395908901076992>');
     }
 
@@ -42,7 +42,7 @@ bot.on('message', (message) => {
 
 bot.on('message', (message) => {
 
-    if(message.content == 'give me scrap prices') {
+    if(message.content == 'h!prices scrap') {
         message.reply('this is what I have. Scrap: **12** <:Bux:329395951003369476>| **300K** <:Gas:329395933949198337> | **850K-1M** <:Minerals:329395908901076992>');
     }
 
@@ -50,7 +50,7 @@ bot.on('message', (message) => {
 
 bot.on('message', (message) => {
 
-    if(message.content == 'give me dark matter prices') {
+    if(message.content == 'h!prices dark matter') {
         message.reply('that is far too rare for my knowledge... I advise asking other captains.');
     }
 
@@ -58,8 +58,20 @@ bot.on('message', (message) => {
 
 bot.on('message', (message) => {
 
-    if(message.content == 'give me unknown material prices') {
+    if(message.content == 'h!prices unknown material') {
         message.reply('that is far too rare for my knowledge... I advise asking other captains.');
+    }
+
+});
+
+bot.on('message', (message) => {
+
+    if(message.content == 'h!prices all') {
+        message.channel.sendMessage('Carbon: **10** <:Bux:329395951003369476>| **200K** <:Gas:329395933949198337> | **400K-600K** <:Minerals:329395908901076992>');
+        message.channel.sendMessage('Scrap: **12** <:Bux:329395951003369476>| **300K** <:Gas:329395933949198337> | **850K-1M** <:Minerals:329395908901076992>');
+        message.channel.sendMessage('Gold: **20** <:Bux:329395951003369476>|*Undetermined* <:Gas:329395933949198337>| **850K-1M** <:Minerals:329395908901076992>');
+        message.channel.sendMessage('Titanium: **9-12** <:Bux:329395951003369476>| **250K** <:Gas:329395933949198337> | **850K** <:Minerals:329395908901076992>');
+        message.channel.sendMessage('Silicon: **25-30** <:Bux:329395951003369476>| **500K** <:Gas:329395933949198337> | **1M** <:Minerals:329395908901076992>');
     }
 
 });
@@ -87,14 +99,14 @@ bot.on('message', (message) => {
 
 bot.on('message', (message) => {
 
-    if(message.content == '#help') {
-        message.reply('I currently don‘t know many commands, since I‘m only a hatchling, but you can say "give me [material] prices" for most materials and I can tell you what my creator told me the average prices are. If you don‘t agree with these please message <@231347753614049282> with suggested market prices');
+    if(message.content == 'h!help') {
+        message.reply('I currently don‘t know many commands, since I‘m only a hatchling, but you can say "h!prices [material]" for most materials (or all for all available) and I can tell you what my creator told me the average prices are. If you don‘t agree with these please message <@231347753614049282> with suggested market prices');
     }
 });
 
 bot.on('message', (message) => {
 
-    if(message.content == '#pet') {
+    if(message.content == 'h!pet') {
         message.reply('you quietly pet <@243565320571322368>');
     }
 });
